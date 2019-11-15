fe2cae0b2ea0e4f98a27e862c8a86c91fbd82603
AmMilu <hehu@tcd.ie>
2019-11-15 15:10:32 +0000

    api access part 1

--- access.py
+++ access.py
@@ -10,28 +10,48 @@
 #print(y["age"])
 
 from github import Github
-#set up
+import urllib
+import json
+
 ACCESS_TOKEN = '43fa63c2ef98b7dd8e0c76ee4560aee7686960f'
 g = Github(ACCESS_TOKEN+'6')
 
 def search_user():
     user = g.get_user(login='jquery')
-    i = vars(user)
-    p = i["_rawData"]
-    print('\n'.join("%s: %s" % item for item in p.items()))
+    user_information = vars(user)
+    user_rawData = user_information["_rawData"]
+    print('\n'.join("%s: %s" % item for item in user_rawData.items()))
 
 def search_repo():
     repo = g.get_user(login='jquery').get_repos()
-    for i in repo:
-        print(i)
+    repo_information = vars(repo)
+    print('\n'.join("%s: %s" % item for item in repo_information.items()))
 
 def search_sepcific_repo():
     repo = g.get_user(login='jquery').get_repo('jquery')
-    print(repo)
+    repo_information = vars(repo)
+    repo_rawData = repo_information["_rawData"]
+    print('\n'.join("%s: %s" % item for item in repo_rawData.items()))
 
 def search_commit():
-    commit = g.get_user(login='jquery').search_commit(query='jquery')
-    print(commit)
+    commit = g.get_repo('jquery/jquery').get_commits()
+    commit_information = vars(commit)
+    #print('\n'.join("%s: %s" % item for item in commit_information.items()))
+    commit_url = commit_information["_PaginatedList__firstUrl"]
+    commit_content = urllib.request.urlopen(commit_url)
+    commit_test = commit_content.read().decode('utf-8')
+    commit_test = commit_test[1:len(commit_test)-2]
+    commit_part = commit_test.split(',{"sha":')
+    #print (commit_part[0])
+    #test = json.loads(commit_part[0])
+    #print('\n'.join("%s: %s" % item for item in test.items()))
+    convert_to_dict(commit_part[0])
+    print('\n')
+    print(len(commit_part))
+
+def convert_to_dict(string):
+    result = json.loads(string)
+    print('\n'.join("%s: %s" % item for item in result.items()))
 
 if __name__ == '__main__':
-    search_user()
+    search_commit()
