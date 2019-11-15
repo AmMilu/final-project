7ec7d6c64296c9692e7686e502f5dd8996be220e
AmMilu <hehu@tcd.ie>
2019-11-15 12:24:52 +0000

    token test

--- access.py
+++ access.py
@@ -11,9 +11,27 @@
 
 from github import Github
 #set up
-ACCESS_TOKEN = '1fed526d5b1be50ab11c9f159abab6ae0214dfd6'
-g = Github(ACCESS_TOKEN)
-    
+ACCESS_TOKEN = '43fa63c2ef98b7dd8e0c76ee4560aee7686960f'
+g = Github(ACCESS_TOKEN+'6')
+
+def search_user():
+    user = g.get_user(login='jquery')
+    i = vars(user)
+    p = i["_rawData"]
+    print('\n'.join("%s: %s" % item for item in p.items()))
+
+def search_repo():
+    repo = g.get_user(login='jquery').get_repos()
+    for i in repo:
+        print(i)
+
+def search_sepcific_repo():
+    repo = g.get_user(login='jquery').get_repo('jquery')
+    print(repo)
+
+def search_commit():
+    commit = g.get_user(login='jquery').search_commit(query='jquery')
+    print(commit)
 
 if __name__ == '__main__':
-    repo = g.get_repos("jquery")
+    search_user()
